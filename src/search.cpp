8aebeb88610ec1beb887a29e68b44932362d28d7
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-27 05:02:00 -0400

    Try using TT evaluation for eval when TT entry depth > depth.
    Bench: 5775619

--- src/search.cpp
+++ src/search.cpp
@@ -748,6 +748,7 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~4 Elo)
         if (    ttValue != VALUE_NONE
+            &&  tte->depth() > depth
             && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
             eval = ttValue;
     }
